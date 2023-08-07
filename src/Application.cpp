f27f7f37267958d2286c9e87b288108578937246
ElectronPie <tima001f@gmail.com>
2023-08-07 15:38:34 +0300

    Added use of Vertex Array Objects (VAOs)

--- src/Application.cpp
+++ src/Application.cpp
@@ -150,6 +150,11 @@ int main()
         return -1;
     }
 
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if(!window)
@@ -188,12 +193,17 @@ int main()
         2, 3, 0
     };
 
+    GLuint vao;
+    GLCall(glGenVertexArrays(1, &vao));
+    GLCall(glBindVertexArray(vao));
+
     GLuint buffer;
     GLCall(glGenBuffers(1, &buffer));
     GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
     GLCall(glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW));
-    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (const void*)0));
+
     GLCall(glEnableVertexAttribArray(0));
+    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (const void*)0));
 
     GLuint ibo;
     GLCall(glGenBuffers(1, &ibo));
@@ -204,11 +214,16 @@ int main()
     GLCall(GLuint shader = CreateShader(vertexSource, fragmentSource));
     GLCall(glUseProgram(shader));
 
-    float r = 0.0f;
-    float increment = 0.05f;
-
     GLCall(GLint location = glGetUniformLocation(shader, "u_Color"));
     ASSERT(location != -1);
+
+    GLCall(glBindVertexArray(0));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+    GLCall(glUseProgram(0));
+
+    float r = 0.0f;
+    float increment = 0.05f;
 
     /* Loop until the user closes the window */
     while(!glfwWindowShouldClose(window))
@@ -216,12 +231,16 @@ int main()
         /* Render here */
         GLCall(glClear(GL_COLOR_BUFFER_BIT));
 
+        GLCall(glUseProgram(shader));
         GLCall(glUniform4f(location,
             r,
             0.63671875f,
             0.40625f,
             1.0f
         ));
+
+        GLCall(glBindVertexArray(vao));
+
         GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL));
 
         if(r >= 1.0f)
